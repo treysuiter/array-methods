b134f287cb080c10f4463bef5e3c799eb49756b4
Trey Suiter <trey.suiter@gmail.com>
2019-10-28 11:15:19 -0500

    forEach implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -113,3 +113,16 @@ const businesses = [
         addressCity: "Watrous"
     }
 ];
+
+const outEl = document.querySelector("#output")
+outEl.innerHTML = "<h1>Active Businesses</h1>"
+
+businesses.forEach(business => {
+  outEl.innerHTML += `
+    <h2>${business.companyName}</h2>
+    <section>
+      ${business.addressFullStreet}
+    </section>
+  `
+  outEl.innerHTML += "<hr/>"
+});
