b604a944f4c6360f1e599ab3d38406412800ba51
Trey Suiter <trey.suiter@gmail.com>
2019-10-28 12:59:17 -0500

    used map method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -192,3 +192,21 @@ const businesses = [
 //   outEl.innerHTML += "<hr/>"
 // });
 
+const outEl = document.querySelector("#output")
+outEl.innerHTML += "<h1>Purchasing Agents</h1>";
+
+/*
+    Using map(), you extract the purchasing agent object
+    from each business and store it in a new array
+*/
+const agents = businesses.map(business => {
+    return business.purchasingAgent
+})
+
+console.table(agents)
+
+agents.forEach(agent => {
+  outEl.innerHTML += `<h2>${agent.nameFirst} ${agent.nameLast}</h2>`;
+  outEl.innerHTML += "<hr/>";
+});
+
