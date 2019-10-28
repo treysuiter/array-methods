2a5ef44c3e7d350900a17466fe6d51626c7c4cc9
Trey Suiter <trey.suiter@gmail.com>
2019-10-28 15:57:12 -0500

    completed big spenders

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -114,159 +114,28 @@ const businesses = [
     }
 ];
 
-
-// sets outEl to select dom container #output and attaches h1
-
-// const outEl = document.querySelector("#output")
-// outEl.innerHTML = "<h1>Active Businesses</h1>"
-
-//initial forEach loop to print all entires to the DOM
-
-
-
-// businesses.forEach(business => {
-//     const zipCodeKey = "addressZipCode"
-//   outEl.innerHTML += `
-//     <h2>${business.companyName}</h2>
-//     <section>
-//       ${business.addressFullStreet}
-//     </section>
-//       ${business.addressCity}, ${business.addressStateCode} ${business[zipCodeKey]}
-//     </section>
-//   `
-//   outEl.innerHTML += "<hr/>"
-// });
-
-
-// used .filer to search for businesses in NY, returns true if so, then prints them to the DOM
-
-// const newYorkBusinesses = businesses.filter(business => {
-//     let inNewYork = false
-
-//     if (business.addressStateCode === "NY") {
-//         inNewYork = true
-//     }
-
-//     return inNewYork
-// })
-
-// console.log(newYorkBusinesses)
-
-// newYorkBusinesses.forEach(business => {
-//     const zipCodeKey = "addressZipCode"
-//   outEl.innerHTML += `
-//     <h2>${business.companyName}</h2>
-//     <section>
-//       ${business.addressFullStreet}
-//     </section>
-//       ${business.addressCity}, ${business.addressStateCode} ${business[zipCodeKey]}
-//     </section>
-//   `
-//   outEl.innerHTML += "<hr/>"
-// });
-
-// const outEl = document.querySelector("#output")
-// outEl.innerHTML = "<h1>Manufacturing Businesses</h1>"
-
-// const manufacturingBusinesses = businesses.filter(business => {
-//     let inNewYork = false
-
-//     if (business.companyIndustry === "Manufacturing") {
-//         inNewYork = true
-//     }
-
-//     return inNewYork
-// })
-
-
-// manufacturingBusinesses.forEach(business => {
-//     const zipCodeKey = "addressZipCode"
-//   outEl.innerHTML += `
-//     <h2>${business.companyName}</h2>
-//     <section>
-//       ${business.addressFullStreet}
-//     </section>
-//       ${business.addressCity}, ${business.addressStateCode} ${business[zipCodeKey]}
-//     </section>
-//   `
-//   outEl.innerHTML += "<hr/>"
-// });
-
-// const outEl = document.querySelector("#output")
-// outEl.innerHTML += "<h1>Purchasing Agents</h1>";
-
-// /*
-//     Using map(), you extract the purchasing agent object
-//     from each business and store it in a new array
-// */
-// const agentsWithCompanyAndPhone = businesses.map(business => {
-//     return {
-//         "fullName": `${business.purchasingAgent.nameFirst} ${business.purchasingAgent.nameFirst}`,
-//         "company": business.companyName,
-//         "phoneNumber": business.phoneWork
-//     }
-// })
-
-// // console.table(agents)
-
-// agentsWithCompanyAndPhone.forEach(agent => {
-//     outEl.innerHTML += `<h2>${agent.fullName}</h2>`
-//     outEl.innerHTML += `<p>${agent.company}`
-//     outEl.innerHTML += `<p>${agent.phoneNumber}</p>`
-//     outEl.innerHTML += "<hr/>"
-// });
-
-// const outEl = document.querySelector("#output")
-
-// document.querySelector("#companySearch")
-//     .addEventListener("keypress", keyPressEvent => {
-//         if (keyPressEvent.charCode === 13) {
-//             // console.log("enter key pressed")
-//             /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
-
-//             const foundBusiness = businesses.find(
-                
-//                 business => business.purchasingAgent.nameFirst.includes(keyPressEvent.target.value) || business.purchasingAgent.nameLast.includes(keyPressEvent.target.value)
-//             );
-
-//             outEl.innerHTML = `
-//                 <h2>
-//                 ${foundBusiness.purchasingAgent.nameFirst} ${foundBusiness.purchasingAgent.nameLast}
-//                 </h2>
-//                 <section>
-               
-//             `;
-//         }
-//     });
-
 const outEl = document.querySelector("#output")
 
+// Array to contain all the big spenders
+const bigSpenders = businesses.filter(business => {
 
+    let above9k = false
 
-businesses.forEach(business => {
-    /* CALCULATE ORDER SUMMARY */
-    // let totalOrders = 0
-    // business.orders.forEach(order => totalOrders += order)
-
-    let totalOrders = business.orders.reduce(
-        (currentTotal, nextValue) => currentTotal += nextValue,
-        0
+    business.orders.forEach(amount => {
+        if (amount > 9000) {
+            above9k = true
+        }
+    }
     )
+    return above9k
+})
 
+console.log(bigSpenders)
 
-    outEl.innerHTML += `
-        <h2>
-            ${business.companyName}
-            ($${totalOrders})
-        </h2>
-        <section>
-            ${business.addressFullStreet}
-        </section>
-        <section>
-            ${business.addressCity},
-            ${business.addressStateCode}
-            ${business.addressZipCode}
-        </section>
-    `;
-    outEl.innerHTML += "<hr/>";
-});
+bigSpenders.forEach(spender => {
+    const toDOM = `
+    <h3>${spender.companyName}</h3>
+    `
+    outEl.innerHTML += toDOM
+})
+
